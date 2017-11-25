63dc6cd527cba2df22ba3a8b9c6446a4b579a626
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2017-11-25 20:26:30 +0100

    Randomize struct

--- scripts/captains-log.go
+++ scripts/captains-log.go
@@ -3,23 +3,48 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 type Signal struct {
-	to   string
-	from string
+	to      string
+	from    string
+	message string
+	testId  string
 }
 
 func generateSignal() Signal {
+
+	names := []string{
+		"Alice",
+		"Bjorn",
+		"Cilla",
+		"Dylan",
+	}
+	to := names[rand.Intn(len(names))]
+	from := names[rand.Intn(len(names))]
+
+	messages := []string{
+		"Hello!",
+		"Who are you?",
+		"I am " + from,
+		"My name is " + from,
+		"Goodbye",
+	}
+
 	message := Signal{
-		to:   "Magnus",
-		from: "Bjorn",
+		to:      to,
+		from:    from,
+		message: messages[rand.Intn(len(messages))],
+		testId:  "DSA",
 	}
 
 	return message
 }
 
 func main() {
+	rand.Seed(time.Now().Unix())
 	fmt.Println("Captain's log:\n")
 	signal := generateSignal()
 	b, err := json.Marshal(signal)
