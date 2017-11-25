9e0a68bebd2a734891e5e5f97e5ca6ed4ba0c0e3
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2017-11-25 19:24:20 +0100

    Add signal struct

--- scripts/captains-log.go
+++ scripts/captains-log.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
+type Signal struct {
+	to   string
+	from string
+}
+
+func generateSignal() Signal {
+	message := Signal{
+		to:   "Magnus",
+		from: "Bjorn",
+	}
+
+	return message
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Captain's log:\n")
+	signal := generateSignal()
+	b, err := json.Marshal(signal)
+
+	if err != nil {
+		fmt.Println("Error encoding:", err.Error())
+	}
+
+	fmt.Println(signal)
+	fmt.Println(b)
 }
